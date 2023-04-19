0de013d955fad46b2acd94fc0a71bc4ca2a392d5
bujosa <davidbujosa@gmail.com>
2023-04-19 17:15:41 -0400

    feat: add example calling api, and using async and await

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
-use error_chain::error_chain;
+use reqwest::header::USER_AGENT;
+use reqwest::Error;
+use serde::Deserialize;
 
-error_chain! {
-    foreign_links {
-        Io(std::io::Error);
-        HttpRequest(reqwest::Error);
-    }
+#[derive(Deserialize, Debug)]
+struct User {
+    login: String,
+    id: u32,
 }
 
 #[tokio::main]
-async fn main() -> Result<()> {
+async fn main() -> Result<(), Error> {
+    // Make a GET request to https://www.rust-lang.org
     let resp = reqwest::get("https://www.rust-lang.org")
         .await?
         .text()
@@ -16,5 +18,29 @@ async fn main() -> Result<()> {
 
     println!("Response: {}", resp);
 
+    fetch_data().await?;
+
+    Ok(())
+}
+
+async fn fetch_data() -> Result<(), Error> {
+    let request_url = format!(
+        "https://api.github.com/repos/{owner}/{repo}/stargazers",
+        owner = "rust-lang-nursery",
+        repo = "rust-cookbook"
+    );
+    println!("Requesting {}", request_url);
+
+    let client = reqwest::Client::new();
+    let response = client
+        .get(&request_url)
+        .header(USER_AGENT, "rust web-api-client demo")
+        .send()
+        .await?;
+
+    let users: Vec<User> = response.json().await?;
+    for user in users {
+        println!("{} ({})", user.login, user.id);
+    }
     Ok(())
 }
