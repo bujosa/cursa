36df87055d51ad36bc4e8bc022a7dfe56f699e25
bujosa <davidbujosa@gmail.com>
2023-04-19 02:35:38 -0400

    feat: add simple example of using async and await, using tokio library

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # cursa
 This is a simple project in rust about async and await
+
+## How to run
+```
+cargo run
+```
+
+## How to test
+```
+cargo test
+```
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use error_chain::error_chain;
+
+error_chain! {
+    foreign_links {
+        Io(std::io::Error);
+        HttpRequest(reqwest::Error);
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let resp = reqwest::get("https://www.rust-lang.org")
+        .await?
+        .text()
+        .await?;
+
+    println!("Response: {}", resp);
+
+    Ok(())
 }
